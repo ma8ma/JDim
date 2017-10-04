fe21351e5ca71b620960bfaf4c7d1988ea614c1d
JD Project <jd.project@acc574344b8506f1335297eaa7f74be0f7ea992b>
2017-10-04 21:18:50 +0900

    [WIP] feat: Add IDHASH for NodeTreeBase
    独自のハッシュテーブルを使ってIDをカウントする。

--- src/jdlib/misctrip.h
+++ src/jdlib/misctrip.h
@@ -13,6 +13,8 @@ namespace MISC
 {
     // トリップを取得 (SHA1等の新方式対応)
     std::string get_trip( const std::string& str, const CharCode charcode );
+    // FNV Hash
+    uint32_t fnv_hash( const char *key, size_t length );
 }
 
 #endif
