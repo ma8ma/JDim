11f969e1de8aeb51ae702e5b78669782dc979538
JD Project <jd.project@acc574344b8506f1335297eaa7f74be0f7ea992b>
2017-10-04 21:18:50 +0900

    [WIP] feat: Add MISC::replace_casestr()
    大文字小文字を無視して文字列を置換する関数を追加する。
    NOTE: この関数はReplaceStr_Managerで使う。

--- src/jdlib/miscutil.h
+++ src/jdlib/miscutil.h
@@ -95,6 +95,9 @@ namespace MISC
     /// pattern を replacement に置き換える
     std::string replace_str( std::string_view str, std::string_view pattern, std::string_view replacement );
 
+    // str1 を str2 に置き換え( ignore case 版 )
+    std::string replace_casestr( const std::string& str, const std::string& str1, const std::string& str2 );
+
     /// list_inから pattern を replacement に置き換えてリストを返す
     std::list<std::string> replace_str_list( const std::list<std::string>& list_in,
                                              std::string_view pattern, std::string_view replacement );
