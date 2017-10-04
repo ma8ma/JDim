57ea4ff7bcc5bcc65be484f0df498555f0d451df
JD Project <jd.project@acc574344b8506f1335297eaa7f74be0f7ea992b>
2017-10-04 21:18:50 +0900

    [WIP] feat: Update jdversion.h to a171004

--- src/jdversion.h
+++ src/jdversion.h
@@ -17,8 +17,8 @@
 #define MAJORVERSION 0
 #define MINORVERSION 3
 #define MICROVERSION 0
-#define JDDATE_FALLBACK    "20200118"
-#define JDTAG     ""
+#define JDDATE_FALLBACK    "171004"
+#define JDTAG     "a"
 
 //---------------------------------
 
