9b8e114fc837c806ed4b790974cfc1222a89ffbe
JD Project <jd.project@acc574344b8506f1335297eaa7f74be0f7ea992b>
2017-10-04 21:18:50 +0900

    [WIP] feat,refactor: Update function parameters for ARTICLE::set_width_of_char()
    文字幅を登録するUnicodeコードポイントをutf-8バイト列＆サイズ渡しから
    utf-32コードポイント渡しに変更する。

--- src/article/font.cpp
+++ src/article/font.cpp
@@ -5,8 +5,6 @@
 
 #include "font.h"
 
-#include "jdlib/miscutil.h"
-
 #include "fontid.h"
 #include "config/globalconf.h"
 
@@ -27,10 +25,7 @@ struct WIDTH_DATA
 static WIDTH_DATA* width_of_char[ FONT_NUM ];
 static bool strict_of_char = false;
 
-enum
-{
-    NUM_CHAR_MAX = 0x110000 // フォントの幅を記録する文字数
-};
+constexpr char32_t NUM_CHAR_MAX = 0x110000;  // フォントの幅を記録する文字数
 
 
 //
@@ -129,19 +124,18 @@ bool ARTICLE::get_width_of_char( const char32_t code, const char pre_char, int& 
 //
 // width == -1 はフォント幅の取得に失敗した場合
 //
-void ARTICLE::set_width_of_char( const char* utfstr, int& byte, const char pre_char, const int width, const int width_wide, const int mode )
-{    
-    const int ucs2 = MISC::utf8toucs2( utfstr, byte );
-    if( ! byte ) return;
-    if( ucs2 >= NUM_CHAR_MAX ) return;
+void ARTICLE::set_width_of_char( const char32_t code, const char pre_char, const int width, const int width_wide,
+                                 const int mode )
+{
+    if( code >= NUM_CHAR_MAX ) return;
 
     // 半角モードの幅を厳密に求める場合
-    if( byte == 1 && strict_of_char ){
+    if( code < 128 && strict_of_char ){
 
-        const int pre_char_num = ( int ) pre_char;
-        if( pre_char_num < 128 ) width_of_char[ mode ][ ucs2 ].width[ pre_char_num ] = width;
+        const int pre_char_num = pre_char;
+        if( pre_char_num < 128 ) width_of_char[ mode ][ code ].width[ pre_char_num ] = width;
     }
 
     // 全角モードの幅
-    width_of_char[ mode ][ ucs2 ].width_wide = width_wide;
+    width_of_char[ mode ][ code ].width_wide = width_wide;
 }
