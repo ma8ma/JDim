af8878ba2f4450d4003b3c099dd1a591c6bf084a
JD Project <jd.project@acc574344b8506f1335297eaa7f74be0f7ea992b>
2017-10-04 21:18:50 +0900

    [IDEA] replacestr: Add ignore case option
    大文字小文字を区別しないオプションを追加する。
    正規表現を使わないときは無効にする。

--- src/replacestrpref.cpp
+++ src/replacestrpref.cpp
@@ -28,6 +28,7 @@ ReplaceStrDiag::ReplaceStrDiag( Gtk::Window* parent, ReplaceStrCondition conditi
     , m_check_active( "有効(_E)", true )
     , m_check_icase( "大文字小文字(_I)", true )
     , m_check_regex( "正規表現(_R)", true )
+    , m_check_wchar( "全角半角(_W)", true )
     , m_label_pattern( "置換パターン(_P)：", true )
     , m_label_replace( "置換文字列(_S)：", true )
 {
@@ -40,13 +41,17 @@ ReplaceStrDiag::ReplaceStrDiag( Gtk::Window* parent, ReplaceStrCondition conditi
     m_check_icase.set_active( condition.icase );
     m_check_icase.set_sensitive( condition.regex );
     m_check_regex.set_active( condition.regex );
+    m_check_wchar.set_active( condition.wchar );
+    m_check_wchar.set_sensitive( condition.regex );
 
     m_check_active.set_tooltip_text( "この条件の置換を有効にする" );
     m_check_icase.set_tooltip_text( "大文字小文字を区別しない" );
     m_check_regex.set_tooltip_text( "正規表現を使用する" );
+    m_check_wchar.set_tooltip_text( "英数字とカナの種類(俗にいう全角半角)を区別しない" );
 
     m_hbox_regex.pack_start( m_check_regex, Gtk::PACK_SHRINK );
     m_hbox_regex.pack_start( m_check_icase, Gtk::PACK_SHRINK );
+    m_hbox_regex.pack_start( m_check_wchar, Gtk::PACK_SHRINK );
 
     m_entry_pattern.set_text( pattern );
     m_entry_replace.set_text( replace );
@@ -85,6 +90,7 @@ ReplaceStrCondition ReplaceStrDiag::get_condition() const
     condition.active = get_active();
     condition.icase = get_icase();
     condition.regex = get_regex();
+    condition.wchar = get_wchar();
 
     return condition;
 }
@@ -108,6 +114,7 @@ void ReplaceStrDiag::slot_copy()
 void ReplaceStrDiag::slot_sens()
 {
     m_check_icase.set_sensitive( get_regex() );
+    m_check_wchar.set_sensitive( get_regex() );
 }
 
 
@@ -162,10 +169,13 @@ ReplaceStrPref::ReplaceStrPref( Gtk::Window* parent, const std::string& url )
     columns[2] = Gtk::manage( new Gtk::TreeViewColumn( "大小", m_columns.m_col_icase ) );
     columns[2]->set_fixed_width( 35 );
     columns[2]->set_alignment( Gtk::ALIGN_CENTER );
-    columns[3] = Gtk::manage( new Gtk::TreeViewColumn( "置換パターン", m_columns.m_col_pattern ) );
-    columns[3]->set_fixed_width( 220 );
-    columns[4] = Gtk::manage( new Gtk::TreeViewColumn( "置換文字列", m_columns.m_col_replace ) );
-    columns[4]->set_fixed_width( 200 );
+    columns[3] = Gtk::manage( new Gtk::TreeViewColumn( "全角", m_columns.m_col_wchar ) );
+    columns[3]->set_fixed_width( 35 );
+    columns[3]->set_alignment( Gtk::ALIGN_CENTER );
+    columns[4] = Gtk::manage( new Gtk::TreeViewColumn( "置換パターン", m_columns.m_col_pattern ) );
+    columns[4]->set_fixed_width( 220 );
+    columns[5] = Gtk::manage( new Gtk::TreeViewColumn( "置換文字列", m_columns.m_col_replace ) );
+    columns[5]->set_fixed_width( 200 );
 
     for( Gtk::TreeViewColumn* col : columns ) {
         col->set_sizing( Gtk::TREE_VIEW_COLUMN_FIXED );
@@ -250,6 +260,7 @@ void ReplaceStrPref::append_row( const Glib::RefPtr<Gtk::ListStore>& store, Repl
         row[ m_columns.m_col_active ] = condition.active;
         row[ m_columns.m_col_icase ] = condition.icase;
         row[ m_columns.m_col_regex ] = condition.regex;
+        row[ m_columns.m_col_wchar ] = condition.wchar;
         row[ m_columns.m_col_pattern ] = pattern;
         row[ m_columns.m_col_replace ] = replace;
 
@@ -314,9 +325,10 @@ void ReplaceStrPref::slot_ok_clicked()
             const bool active = row[ m_columns.m_col_active ];
             const bool icase = row[ m_columns.m_col_icase ];
             const bool regex = row[ m_columns.m_col_regex ];
-            constexpr bool reserved = false; // wchar, norm
+            const bool wchar = row[ m_columns.m_col_wchar ];
+            constexpr bool reserved = false; // norm
 
-            const ReplaceStrCondition condition{ active, icase, regex, reserved, reserved };
+            const ReplaceStrCondition condition{ active, icase, regex, wchar, reserved };
             const Glib::ustring& pattern = row[ m_columns.m_col_pattern ];
             const Glib::ustring& replace = row[ m_columns.m_col_replace ];
 
@@ -344,14 +356,16 @@ void ReplaceStrPref::slot_row_activated( const Gtk::TreeModel::Path& path, Gtk::
     const bool active = row[ m_columns.m_col_active ];
     const bool icase = row[ m_columns.m_col_icase ];
     const bool regex = row[ m_columns.m_col_regex ];
-    constexpr bool reserved = false; // wchar, norm
-    const ReplaceStrCondition condition{ active, icase, regex, reserved, reserved };
+    const bool wchar = row[ m_columns.m_col_wchar ];
+    constexpr bool reserved = false; // norm
+    const ReplaceStrCondition condition{ active, icase, regex, wchar, reserved };
 
     ReplaceStrDiag dlg( this, condition, row[ m_columns.m_col_pattern ], row[ m_columns.m_col_replace ] );
     if( dlg.run() == Gtk::RESPONSE_OK ) {
         row[ m_columns.m_col_active ] = dlg.get_active();
         row[ m_columns.m_col_icase ] = dlg.get_icase();
         row[ m_columns.m_col_regex ] = dlg.get_regex();
+        row[ m_columns.m_col_wchar ] = dlg.get_wchar();
         row[ m_columns.m_col_pattern ] = dlg.get_pattern();
         row[ m_columns.m_col_replace ] = dlg.get_replace();
 
@@ -360,7 +374,7 @@ void ReplaceStrPref::slot_row_activated( const Gtk::TreeModel::Path& path, Gtk::
             constexpr bool newline = true;
             constexpr bool migemo = false;
 
-            if( ! ptn.set( dlg.get_pattern(), dlg.get_icase(), newline, migemo ) ) {
+            if( ! ptn.set( dlg.get_pattern(), dlg.get_icase(), newline, migemo, dlg.get_wchar() ) ) {
                 const std::string msg = ptn.errstr() + "\n\n" + dlg.get_pattern();
 
                 SKELETON::MsgDiag mdlg( *this, msg, false, Gtk::MESSAGE_WARNING, Gtk::BUTTONS_OK );
