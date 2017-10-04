bbabc36e424ad2452e10199924d8dd386b6dd971
JD Project <jd.project@acc574344b8506f1335297eaa7f74be0f7ea992b>
2017-10-04 21:18:50 +0900

    Revert "[BUG] Update function parameters for NodeTreeBase::parse_html()"
    This reverts commit a727aca509c3f41f197f44086ba0ab5ae593fb6f.
    XXX: 5chのログの画像リンクを機能させるためにコミットを取り消す。

--- src/dbtree/nodetreebase.cpp
+++ src/dbtree/nodetreebase.cpp
@@ -1045,7 +1045,8 @@ NODE* NodeTreeBase::append_html( const std::string& html )
 
     const bool digitlink = false;
     const bool bold = false;
-    parse_html( html.c_str(), html.length(), COLOR_CHAR, digitlink, bold );
+    const bool ahref = true;
+    parse_html( html.c_str(), html.length(), COLOR_CHAR, digitlink, bold, ahref );
 
     clear();
 
@@ -2061,7 +2062,8 @@ void NodeTreeBase::parse_date_id( NODE* header, const char* str, const int lng )
 //
 // bold : ボールド表示
 //
-// (例) parse_html( "<a href=\"hoge.com\">hoge</a>", 27, COLOR_CHAR, false );
+// ahref : <a href=～></a> からリンクノードを作成する
+// (例) parse_html( "<a href=\"hoge.com\">hoge</a>", 27, COLOR_CHAR, false, false );
 //
 // (パッチ)
 //
@@ -2140,8 +2142,9 @@ create_multispace:
                 && ( *( pos + 2 ) == 'r' || *( pos + 2 ) == 'R' )
                 ) br = true;
 
-            //  <a href=～></a>
-            else if( ( *( pos + 1 ) == 'a' || *( pos + 1 ) == 'A' ) && *( pos + 2 ) == ' ' ){
+            //  ahref == true かつ <a href=～></a>
+            else if( ahref &&
+                     ( *( pos + 1 ) == 'a' || *( pos + 1 ) == 'A' ) && *( pos + 2 ) == ' ' ){
 
                 // フラッシュ
                 create_node_ntext( m_parsed_text.c_str(), m_parsed_text.size(), fgcolor, bgcolor, in_bold, fontid );
