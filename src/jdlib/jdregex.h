79d2bc927188bee52fcb4fbdde10596fa75f5cf4
JD Project <jd.project@acc574344b8506f1335297eaa7f74be0f7ea992b>
2017-10-04 21:18:50 +0900

    [WIP] feat: Update regex class
    - RegexPatternを導入してコンパイル済みの正規表現オブジェクトを
      保持できるように修正する。
    - Glib regex (GRegex)に対応する。
    NOTE: 利用側の修正がないとコンパイルに失敗する。
    
    Regex: Fix table bounds check
    std::vectorの境界を越えてアクセスしている箇所があったため修正します。
    
    再現の手順
    1. コンテナーの境界チェックを有効にしてビルド
    2. http://hayabusa.open2ch.net/test/read.cgi/livejupiter/1593929556/
    3. 「あ」を検索
    4. 異常終了

--- src/jdlib/jdregex.h
+++ src/jdlib/jdregex.h
@@ -27,20 +27,56 @@
 
 namespace JDLIB
 {
+    class Regex;
+
+    class RegexPattern
+    {
+        friend class Regex;
+
+#ifdef POSIX_STYLE_REGEX_API
+        // onigurumaではregexec()の引数regex_t*がconst修飾されていない
+        // そのためRegex::match()のコンパイルエラーを回避するためmutable修飾が必要
+        mutable regex_t m_regex;
+#else
+        GRegex* m_regex{};
+#endif
+        bool m_compiled{};
+        bool m_newline{};
+        bool m_wchar{};
+        bool m_norm;
+#ifdef POSIX_STYLE_REGEX_API
+        int m_error{};
+#else
+        GError *m_error{};
+#endif
+
+    public:
+        RegexPattern() : m_compiled( false ), m_error( 0 ){};
+        RegexPattern( const std::string& reg, const bool icase, const bool newline,
+                        const bool usemigemo = false, const bool wchar = false,
+                        const bool norm = false );
+        ~RegexPattern();
+
+        // m_regexを複製する方法がないためcopy禁止にする
+        RegexPattern( const RegexPattern& ) = delete;
+        RegexPattern& operator=( const RegexPattern& ) = delete;
+        // moveは許可
+        RegexPattern( RegexPattern&& ) noexcept = default;
+        RegexPattern& operator=( RegexPattern&& ) noexcept = default;
+
+        bool set( const std::string& reg, const bool icase, const bool newline,
+                        const bool usemigemo = false, const bool wchar = false,
+                        const bool norm = false );
+        void clear();
+        bool compiled() const { return m_compiled; }
+        std::string errstr() const;
+    };
+
+
     class Regex
     {
         std::vector< int > m_pos;
         std::vector< std::string > m_results;
-
-#ifdef POSIX_STYLE_REGEX_API
-        regex_t m_reg;
-#else
-        GRegex* m_reg{};
-#endif
-
-        bool m_compiled{};
-        bool m_newline{};
-        bool m_wchar{};
 
         // 全角半角を区別しないときに使う変換用バッファ
         // 処理可能なバッファ長は regoff_t (= int) のサイズに制限される
@@ -49,24 +85,30 @@ namespace JDLIB
 
     public:
 
-        Regex() = default;
-        ~Regex();
+        Regex() noexcept = default;
+        ~Regex() noexcept = default;
 
-        void dispose();
-        
+        // notbol : 行頭マッチは必ず失敗する
+        // noteol : 行末マッチは必ず失敗する
+        bool match( const RegexPattern& creg, const std::string& target, const size_t offset,
+                    const bool notbol = false, const bool noteol = false );
+
         // icase : 大文字小文字区別しない
         // newline :  . に改行をマッチさせない
         // usemigemo : migemo使用 (コンパイルオプションで指定する必要あり)
         // wchar : 全角半角の区別をしない
-        bool compile( const std::string& reg, const bool icase, const bool newline, const bool usemigemo,
-                      const bool wchar );
+        bool exec( const std::string& reg, const std::string& target, const size_t offset,
+                    const bool icase, const bool newline, const bool usemigemo = false,
+                    const bool wchar = false, const bool norm = false ){
+            RegexPattern pattern( reg, icase, newline, usemigemo, wchar, norm );
+            return match( pattern, target, offset );
+        }
 
-        bool exec( const std::string& target, const size_t offset );
-        bool exec( const std::string& reg, const std::string& target, const size_t offset, const bool icase,
-                   const bool newline, const bool usemigemo, const bool wchar );
+        // マッチした文字列と \0〜\9 を置換する
+        std::string replace( const std::string& repstr );
 
-        int pos( const size_t num );
-        std::string str( const size_t num );
+        int pos( const size_t num ) const;
+        std::string str( const size_t num ) const;
     };
 }
 
