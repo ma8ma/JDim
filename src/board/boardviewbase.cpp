007c3718fb6d92fd4ca1fb2dfed55cd98a44db8a
JD Project <jd.project@acc574344b8506f1335297eaa7f74be0f7ea992b>
2017-10-04 21:18:50 +0900

    [WIP] feat: Update thread speed in BoardViewBase common update
    共通の列を更新するメンバー関数でスレッドの速度を更新するよう修正する。

--- src/board/boardviewbase.cpp
+++ src/board/boardviewbase.cpp
@@ -1869,6 +1869,10 @@ void BoardViewBase::update_row_common( const Gtk::TreeModel::Row& row )
         row[ m_columns.m_col_new ] = -1;
     }
 
+    // 速度
+    if( ( art->get_status() & STATUS_NORMAL ) && ! art->is_924() )
+        row[ m_columns.m_col_speed ] = art->get_speed();
+
 
     //
     // マーク
