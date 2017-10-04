c96dee35e92372fc5dabb65e8ba09f8278e599e8
JD Project <jd.project@acc574344b8506f1335297eaa7f74be0f7ea992b>
2017-10-04 21:18:50 +0900

    [WIP] feat: Set cookie when read node tree
    ノードツリーを読み込んだときにcookieをセットするよう修正する。

--- src/dbtree/nodetreebase.cpp
+++ src/dbtree/nodetreebase.cpp
@@ -1427,7 +1427,14 @@ void NodeTreeBase::receive_finish()
 
     if( ! m_check_update
         && ( get_code() == HTTP_OK || get_code() == HTTP_PARTIAL_CONTENT )
-        && ! get_date_modified().empty() ) CACHE::set_filemtime( CACHE::path_dat( m_url ), get_time_modified() );
+        && ! get_date_modified().empty() ){
+
+        CACHE::set_filemtime( CACHE::path_dat( m_url ), get_time_modified() );
+
+        // クッキーのセット
+        const::std::list< std::string > list_cookies = SKELETON::Loadable::cookies();
+        DBTREE::board_set_list_cookies_for_write( m_url, list_cookies );
+    }
 
     m_check_update = false;
     m_check_write = false;
