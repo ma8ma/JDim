2eccb2d866c180f9f0d2af47a0ff7225f619394b
JD Project <jd.project@acc574344b8506f1335297eaa7f74be0f7ea992b>
2017-10-04 21:18:50 +0900

    [WIP] feat: Refactor NodeTreeBase::parse_name()
    デフォルト名無しのチェックを更新する。

--- src/dbtree/nodetreebase.cpp
+++ src/dbtree/nodetreebase.cpp
@@ -1700,15 +1700,15 @@ const char* NodeTreeBase::add_one_dat_line( const char* datline )
 //
 void NodeTreeBase::parse_name( NODE* header, const char* str, const int lng, const int color_name )
 {
-    const bool bold = true;
     const bool ahref = false;
     int lng_name = lng;
     NODE *node;
 
-    const bool defaultname{ m_default_noname.compare( 0, lng, str, lng ) == 0 };
-
     // 後ろの空白を除く
     while( lng_name > 0 && str[ lng_name - 1 ] == ' ' ) --lng_name;
+
+    const bool defaultname = ( m_default_noname.length() == ( size_t )lng_name
+            && m_default_noname.compare( 0, lng_name, str, lng_name ) == 0 );
 
     // 文字列置換
     std::string str_name;
@@ -1735,6 +1735,7 @@ void NodeTreeBase::parse_name( NODE* header, const char* str, const int lng, con
     // デフォルト名無しと同じときはアンカーを作らない
     if( defaultname ){
         constexpr bool digitlink = false;
+        constexpr bool bold = true;
         parse_html( str, lng_name, color_name, digitlink, bold, ahref, FONT_MAIL );
     }
     else{
@@ -1749,17 +1750,19 @@ void NodeTreeBase::parse_name( NODE* header, const char* str, const int lng, con
                 if( str[ i ] == '<'
                     && str[ i+1 ] == '/'
                     && ( str[ i+2 ] == 'b' || str[ i+2 ] == 'B' )
-                    && str[ i+3 ] == '>' ) break;
+                    && str[ i+3 ] == '>' ){ i += 4; break; }
             }
 
             // </b>の前までパース
             if( i != pos ){
                 // デフォルト名無しと同じときはアンカーを作らない
-                const bool digitlink = ( strncmp( m_default_noname.data(), str + pos, i - pos ) != 0 );
+                const size_t len = m_default_noname.size();
+                const bool digitlink = m_default_noname.compare( 0, len, str + pos, len );
+                constexpr bool bold = true;
                 parse_html( str + pos, i - pos, color_name, digitlink, bold, ahref, FONT_MAIL );
             }
             if( i >= lng_name ) break;
-            pos = i + 4; // 4 = strlen( "</b>" );
+            pos = i;
 
             // <b>の位置を探す
             int pos_end = lng_name;
@@ -1783,28 +1786,24 @@ void NodeTreeBase::parse_name( NODE* header, const char* str, const int lng, con
 
             // </b><b>の中をパース
             constexpr bool digitlink = false; // 数字が入ってもリンクしない
+            constexpr bool bold = false;
             parse_html( str + pos, pos_end - pos, COLOR_CHAR_NAME_B, digitlink, bold, ahref, FONT_MAIL );
 
-            pos = pos_end + 3; // 3 = strlen( "<b>" );
+            pos = pos_end;
         }
     }
 
     // plainな名前取得
     // 名前あぼーんや名前抽出などで使用する
-    if( defaultname ){
-        header->headinfo->name = m_heap.heap_alloc<char>( lng_name +2 );
-        std::memcpy( header->headinfo->name, str, lng_name );
-    }
-    else{
-        std::string str_tmp;
+    std::string str_tmp;
+    node = node->next_node;
+    while( node ){
+        if( node->text ) str_tmp += node->text;
         node = node->next_node;
-        while( node ){
-            if( node->text ) str_tmp += node->text;
-            node = node->next_node;
-        }
-        header->headinfo->name = m_heap.heap_alloc<char>( str_tmp.length() +2 );
-        memcpy( header->headinfo->name, str_tmp.c_str(), str_tmp.length() );
     }
+    header->headinfo->name = m_heap.heap_alloc<char>( str_tmp.length() + 1 );
+    memcpy( header->headinfo->name, str_tmp.c_str(), str_tmp.length() );
+    header->headinfo->name[ str_tmp.length() ] = '\0';
 }
 
 
