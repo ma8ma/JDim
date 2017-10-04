9803b79ac16c992ea670af990fa37f48b2ff5599
JD Project <jd.project@acc574344b8506f1335297eaa7f74be0f7ea992b>
2017-10-04 21:18:50 +0900

    [WIP] refactor: Move definitions to header for XML::DomList

--- src/xml/domlist.cpp
+++ src/xml/domlist.cpp
@@ -9,14 +9,6 @@
 using namespace XML;
 
 
-// std::list< Dom* > が代入された場合
-DomList& DomList::operator =( const std::list< Dom* >& list )
-{
-    m_list = list;
-
-    return *this;
-}
-
 // 添字によるアクセス
 Dom* DomList::operator []( const unsigned int n )
 {
@@ -35,43 +27,6 @@ Dom* DomList::operator []( const unsigned int n )
 }
 
 
-// 以下 std::list の主なメンバ関数
-
-std::list< Dom* >::iterator DomList::begin()
-{
-    return m_list.begin();
-}
-
-std::list< Dom* >::reverse_iterator DomList::rbegin()
-{
-    return m_list.rbegin();
-}
-
-std::list< Dom* >::iterator DomList::end()
-{
-    return m_list.end();
-}
-
-std::list< Dom* >::reverse_iterator DomList::rend()
-{
-    return m_list.rend();
-}
-
-size_t DomList::size() const
-{
-    return m_list.size();
-}
-
-size_t DomList::max_size() const
-{
-    return m_list.max_size();
-}
-
-bool DomList::empty() const
-{
-    return m_list.empty();
-}
-
 // 参照ではなくポインタを返す
 Dom* DomList::front()
 {
@@ -87,75 +42,3 @@ Dom* DomList::back()
 
     return *m_list.end();
 }
-
-void DomList::push_front( Dom* dom )
-{
-    m_list.push_front( dom );
-}
-
-void DomList::push_back( Dom* dom )
-{
-    m_list.push_back( dom );
-}
-
-void DomList::pop_front()
-{
-    m_list.pop_front();
-}
-
-void DomList::pop_back()
-{
-    m_list.pop_back();
-}
-
-std::list< Dom* >::iterator DomList::insert( std::list< Dom* >::iterator it, Dom* dom )
-{
-    return m_list.insert( it, dom );
-}
-
-std::list< Dom* >::iterator DomList::erase( std::list< Dom* >::iterator it )
-{
-    return m_list.erase( it );
-}
-
-void DomList::clear()
-{
-    m_list.clear();
-}
-
-void DomList::splice( std::list< Dom* >::iterator it, DomList& domlist  )
-{
-    m_list.splice( it, domlist.m_list );
-}
-
-void DomList::remove( Dom* dom )
-{
-    m_list.remove( dom );
-}
-
-void DomList::unique()
-{
-    m_list.unique();
-}
-
-void DomList::merge( DomList& domlist )
-{
-    m_list.merge( domlist.m_list );
-
-}
-
-void DomList::sort()
-{
-    m_list.sort();
-}
-
-void DomList::reverce()
-{
-    m_list.reverse();
-}
-
-void DomList::swap( DomList& domlist )
-{
-    m_list.swap( domlist.m_list );
-}
-
--- src/xml/domlist.h
+++ src/xml/domlist.h
@@ -29,36 +29,36 @@ namespace XML
         ~DomList() noexcept = default;
 
         // std::list< Dom* > が代入された場合
-        DomList& operator =( const std::list< Dom* >& list );
+        DomList& operator =( const std::list< Dom* >& list ){ m_list = list; return *this; }
 
         // 添字によるアクセス
         Dom* operator []( const unsigned int n );
 
         std::list< Dom* > get_list() const noexcept { return m_list; }
 
-        std::list< Dom* >::iterator begin();
-        std::list< Dom* >::reverse_iterator rbegin();
-        std::list< Dom* >::iterator end();
-        std::list< Dom* >::reverse_iterator rend();
-        size_t size() const;
-        size_t max_size() const;
-        bool empty() const;
+        std::list< Dom* >::iterator begin() noexcept { return m_list.begin(); }
+        std::list< Dom* >::reverse_iterator rbegin() noexcept { return m_list.rbegin(); }
+        std::list< Dom* >::iterator end() noexcept { return m_list.end(); }
+        std::list< Dom* >::reverse_iterator rend() noexcept { return m_list.rend(); }
+        size_t size() const noexcept { return m_list.size(); }
+        size_t max_size() const noexcept { return m_list.max_size(); }
+        bool empty() const noexcept { return m_list.empty(); }
         Dom* front(); // front() と back()は参照ではなくポインタを返す
         Dom* back();
-        void push_front( Dom* dom );
-        void push_back( Dom* dom );
-        void pop_front();
-        void pop_back();
-        std::list< Dom* >::iterator insert( std::list< Dom* >::iterator it, Dom* dom );
-        std::list< Dom* >::iterator erase( std::list< Dom* >::iterator it );
-        void clear();
-        void splice( std::list< Dom* >::iterator it, DomList& domlist );
-        void remove( Dom* dom );
-        void unique();
-        void merge( DomList& domlist );
-        void sort();
-        void reverce();
-        void swap( DomList& domlist );
+        void push_front( Dom* dom ){ m_list.push_front( dom ); }
+        void push_back( Dom* dom ){ m_list.push_back( dom ); }
+        void pop_front(){ m_list.pop_front(); }
+        void pop_back(){ m_list.pop_back(); }
+        std::list< Dom* >::iterator insert( std::list< Dom* >::iterator it, Dom* dom ){ return m_list.insert( it, dom ); }
+        std::list< Dom* >::iterator erase( std::list< Dom* >::iterator it ){ return m_list.erase( it ); }
+        void clear(){ m_list.clear(); }
+        void splice( std::list< Dom* >::iterator it, DomList& domlist ){ m_list.splice( it, domlist.m_list ); }
+        void remove( Dom* dom ){ m_list.remove( dom ); }
+        void unique(){ m_list.unique(); }
+        void merge( DomList& domlist ){ m_list.merge( domlist.m_list ); }
+        void sort(){ m_list.sort(); }
+        void reverce(){ m_list.reverse(); }
+        void swap( DomList& domlist ){ m_list.swap( domlist.m_list ); }
     };
 }
 
