2a1829aeb79e03e892e9dad39a5005dc9b296d48
Masayuki Yamamoto <ma3yuki.8mamo10@gmail.com>
2018-07-19 22:50:43 +0900

    E [gtk3] Use gtk_widget_set_visual() instead of Gtk::Widget::shape_combine_mask()
    Type: compile error
    
    Gtk::Widget::shape_combine_mask() is removed since gtk3.

--- src/skeleton/iconpopup.h
+++ src/skeleton/iconpopup.h
@@ -18,14 +18,21 @@ namespace SKELETON
 
       IconPopup( const int icon_id ) : Gtk::Window( Gtk::WINDOW_POPUP ){
 
-            Glib::RefPtr< Gdk::Pixmap > pixmap;
-            Glib::RefPtr< Gdk::Bitmap > bitmap;
-
             m_pixbuf = ICON::get_icon( icon_id );
             m_img.set( m_pixbuf );
 
+#if GTKMM_CHECK_VERSION(3,0,0)
+            set_decorated( false );
+            set_app_paintable( true );
+            auto visual = get_screen()->get_rgba_visual();
+            gtk_widget_set_visual( GTK_WIDGET( gobj() ), visual->gobj() );
+#else
+            Glib::RefPtr< Gdk::Pixmap > pixmap;
+            Glib::RefPtr< Gdk::Bitmap > bitmap;
+
             m_pixbuf->render_pixmap_and_mask( pixmap, bitmap, 255 );
             shape_combine_mask( bitmap, 0, 0 );
+#endif // GTKMM_CHECK_VERSION(3,0,0)
 
             add( m_img );
             show_all_children();
