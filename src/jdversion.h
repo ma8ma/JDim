a43bb9defd2f32a8dae76509a1215560c475ef16
Masayuki Yamamoto <ma3yuki.8mamo10@gmail.com>
2018-12-08 23:09:41 +0900

    version up : jd-2.92.2-beta20181208
    GTK3サポートを追加する(テスト版)
    
    jd-2.92.1-beta20181201 からの変更点
    
    * リベースしてtestブランチを更新する(37181f0)
    * overrideキーワードを使って仮想関数の上書きを明示する
    * 警告オプション-Wextraを追加(ただし-Wunused-parameterは暫定的に無効)
    * テスト版の修正

--- src/jdversion.h
+++ src/jdversion.h
@@ -18,10 +18,10 @@
 // SEE ALSO: ENVIRONMENT::get_jdversion()
 
 #define MAJORVERSION 2
-#define MINORVERSION 8
-#define MICROVERSION 9
-#define JDDATE_FALLBACK    "180424"
-#define JDTAG     ""
+#define MINORVERSION 92
+#define MICROVERSION 2
+#define JDDATE_FALLBACK    "20181208"
+#define JDTAG     "beta"
 
 //---------------------------------
 
