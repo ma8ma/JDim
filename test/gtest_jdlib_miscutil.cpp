e91755bdf42327d3849f9d36cdc12aa9e944b391
Masayuki Yamamoto <ma3yuki.8mamo10@gmail.com>
2019-03-05 23:09:46 +0900

    Add test cases for MISC::asc() more
    全角平仮名、全角片仮名のテストケースを追加する。

--- test/gtest_jdlib_miscutil.cpp
+++ test/gtest_jdlib_miscutil.cpp
@@ -376,4 +376,154 @@ TEST_F(MISC_AscTest, hankaku_katakana_with_dakuten)
     }
 }
 
+TEST_F(MISC_AscTest, zenkaku_katakana_without_dakuten)
+{
+    std::string out;
+    std::vector< int > table;
+
+    // (半)濁点の付いていない全角カタカナ
+    const char* without_dakuten = (
+        u8"\u30A1\u30A2\u30A3\u30A4\u30A5\u30A6\u30A7\u30A8\u30A9\u30AA" // ァ - オ
+        u8"\u30AB\u30AD\u30AF\u30B1\u30B3\u30B5\u30B7\u30B9\u30BB\u30BD" // カ - ソ
+        u8"\u30BF\u30C1\u30C3\u30C4\u30C6\u30C8\u30CA\u30CB\u30CC\u30CD\u30CE" // タ - ノ
+        u8"\u30CF\u30D2\u30D5\u30D8\u30DB\u30DE\u30DF\u30E0\u30E1\u30E2" // ハ - モ
+        u8"\u30E3\u30E4\u30E5\u30E6\u30E7\u30E8\u30E9\u30EA\u30EB\u30EC\u30ED" // ャ - ロ
+        u8"\u30EE\u30EF\u30F0\u30F1\u30F2\u30F3\u30F5\u30F6" // ヮ - ヶ
+    );
+    MISC::asc( without_dakuten, out, table );
+    EXPECT_EQ( without_dakuten, out );
+    EXPECT_EQ( out.size(), table.size() );
+    for( int i = 0, size = out.size(); i < size; ++i ) {
+        EXPECT_EQ( i, table[ i ] );
+    }
+}
+
+TEST_F(MISC_AscTest, zenkaku_katakana_with_dakuten)
+{
+    // TODO: Unicodeの正規化を参考に濁点、半濁点を分解する
+    std::string out;
+    std::vector< int > table;
+
+    // 濁点付き全角カタカナ
+    const char* with_dakuten = (
+        u8"\u30AC\u30AE\u30B0\u30B2\u30B4\u30B6\u30B8\u30BA\u30BC\u30BE" // ガ - ゾ
+        u8"\u30C0\u30C2\u30C5\u30C7\u30C9\u30D0\u30D3\u30D6\u30D9\u30DC" // ダ - ボ
+        u8"\u30F4\u30F7\u30F8\u30F9\u30FA" // ヴ - ヺ
+    );
+    MISC::asc( with_dakuten, out, table );
+    EXPECT_EQ( with_dakuten, out );
+    EXPECT_EQ( out.size(), table.size() );
+    for( int i = 0, size = out.size(); i < size; ++i ) {
+        EXPECT_EQ( i, table[ i ] );
+    }
+}
+
+TEST_F(MISC_AscTest, zenkaku_katakana_with_handakuten)
+{
+    // TODO: Unicodeの正規化を参考に濁点、半濁点を分解する
+    std::string out;
+    std::vector< int > table;
+
+    // 半濁点付き全角カタカナ
+    const char* with_handakuten = u8"\u30D1\u30D4\u30D7\u30DA0x30DD"; // パ - ポ
+    MISC::asc( with_handakuten, out, table );
+    EXPECT_EQ( with_handakuten, out );
+    EXPECT_EQ( out.size(), table.size() );
+    for( int i = 0, size = out.size(); i < size; ++i ) {
+        EXPECT_EQ( i, table[ i ] );
+    }
+}
+
+TEST_F(MISC_AscTest, zenkaku_katakana_precomposed)
+{
+    std::string out;
+    std::vector< int > table, expect_table;
+
+    // 合成済み文字 (半)濁点付き全角カタカナ : カ゚キ゚ク゚ケ゚コ゚
+    // NOTE: 組み合わせが多いのでテストは網羅していない
+    out.clear();
+    table.clear();
+    MISC::asc( u8"\u30AB\u309A\u30AD\u309A\u30AF\u309A\u30B1\u309A\u30B3\u309A", out, table );
+    EXPECT_EQ( u8"\u30AB\u309A\u30AD\u309A\u30AF\u309A\u30B1\u309A\u30B3\u309A", out );
+    EXPECT_EQ( out.size(), table.size() );
+    for( int i = 0, size = out.size(); i < size; ++i ) {
+        EXPECT_EQ( i, table[ i ] );
+    }
+}
+
+TEST_F(MISC_AscTest, zenkaku_hiragana_without_dakuten)
+{
+    std::string out;
+    std::vector< int > table;
+
+    // (半)濁点の付いていない全角ひらがな
+    const char* without_dakuten = (
+        u8"\u3041\u3042\u3043\u3044\u3045\u3046\u3047\u3048\u3049\u304A" // ぁ - お
+        u8"\u304B\u304D\u304F\u3051\u3053\u3055\u3057\u3059\u305B\u305D" // か - そ
+        u8"\u305F\u3061\u3063\u3064\u3066\u3068\u306A\u306B\u306C\u306D\u306E" // た - の
+        u8"\u306F\u3072\u3075\u3078\u307B\u307E\u307F\u3080\u3081\u3082" // は - も
+        u8"\u3083\u3084\u3085\u3086\u3087\u3088\u3089\u308A\u308B\u308C\u308D" // ゃ - ろ
+        u8"\u308E\u308F\u3090\u3091\u3092\u3093\u3095\u3096" // ゎ - ゖ
+    );
+    MISC::asc( without_dakuten, out, table );
+    EXPECT_EQ( without_dakuten, out );
+    EXPECT_EQ( out.size(), table.size() );
+    for( int i = 0, size = out.size(); i < size; ++i ) {
+        EXPECT_EQ( i, table[ i ] );
+    }
+}
+
+TEST_F(MISC_AscTest, zenkaku_hiragana_with_dakuten)
+{
+    // TODO: Unicodeの正規化を参考に濁点、半濁点を分解する
+    std::string out;
+    std::vector< int > table;
+
+    // 濁点付き全角ひらがな
+    const char* with_dakuten = (
+        u8"\u304C\u304E\u3050\u3052\u3054\u3056\u3058\u305A\u305C\u305E" // が - ぞ
+        u8"\u3060\u3062\u3065\u3067\u3069\u3070\u3073\u3076\u3079\u307C" // だ - ぼ
+        u8"\u3094" // ゔ
+    );
+    MISC::asc( with_dakuten, out, table );
+    EXPECT_EQ( with_dakuten, out );
+    EXPECT_EQ( out.size(), table.size() );
+    for( int i = 0, size = out.size(); i < size; ++i ) {
+        EXPECT_EQ( i, table[ i ] );
+    }
+}
+
+TEST_F(MISC_AscTest, zenkaku_hiragana_with_handakuten)
+{
+    // TODO: Unicodeの正規化を参考に濁点、半濁点を分解する
+    std::string out;
+    std::vector< int > table;
+
+    // 半濁点付き全角ひらがな
+    const char* with_handakuten = u8"\u3071\u3074\u3077\u307A0x307D"; // パ - ポ
+    MISC::asc( with_handakuten, out, table );
+    EXPECT_EQ( with_handakuten, out );
+    EXPECT_EQ( out.size(), table.size() );
+    for( int i = 0, size = out.size(); i < size; ++i ) {
+        EXPECT_EQ( i, table[ i ] );
+    }
+}
+
+TEST_F(MISC_AscTest, zenkaku_hiragana_precomposed)
+{
+    std::string out;
+    std::vector< int > table, expect_table;
+
+    // 合成済み文字 (半)濁点付き全角ひらがな : か゚き゚く゚け゚こ゚
+    // NOTE: 組み合わせが多いのでテストは網羅していない
+    out.clear();
+    table.clear();
+    MISC::asc( u8"\u304B\u309A\u304D\u309A\u304F\u309A\u3051\u309A\u3053\u309A", out, table );
+    EXPECT_EQ( u8"\u304B\u309A\u304D\u309A\u304F\u309A\u3051\u309A\u3053\u309A", out );
+    EXPECT_EQ( out.size(), table.size() );
+    for( int i = 0, size = out.size(); i < size; ++i ) {
+        EXPECT_EQ( i, table[ i ] );
+    }
+}
+
 } // namespace
