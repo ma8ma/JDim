f5401eb971907c0985b221e197eb08fe0ddddc42
Masayuki Yamamoto <ma3yuki.8mamo10@gmail.com>
2019-05-29 21:51:12 +0900

    [WIP] refactor: Use std::unique_ptr instead of new/delete for JDLIB::Iconv

--- src/jdlib/miscutil.cpp
+++ src/jdlib/miscutil.cpp
@@ -13,12 +13,15 @@
 #include "dbtree/spchar_decoder.h"
 #include "dbtree/node.h"
 
+#include "cssmanager.h"
+
 #include <glib.h>
 
-#include <sstream>
-#include <cstring>
 #include <cstdio>
 #include <cstdlib>
+#include <cstring>
+#include <memory>
+#include <sstream>
 
 
 //
@@ -1178,8 +1181,8 @@ std::string MISC::url_encode( const char* str, const size_t n, const CharCode co
     }
 
     std::string str_encoded;
-    JDLIB::Iconv* icv = nullptr;
-    if( coding != CHARCODE_UTF8 ) icv = new JDLIB::Iconv( coding, CHARCODE_UTF8 );
+    std::unique_ptr<JDLIB::Iconv> icv;
+    if( coding != CHARCODE_UTF8 ) icv = std::make_unique<JDLIB::Iconv>( coding, CHARCODE_UTF8 );
 
     size_t pos = 0;
     while( pos < n ){
@@ -1205,7 +1208,7 @@ std::string MISC::url_encode( const char* str, const size_t n, const CharCode co
             }
         }
 
-        unsigned char c = str[ pos ];
+        const unsigned char c = str[ pos ];
 
         // 非予約文字はそのまま
         if( ( 'a' <= c && c <= 'z' ) || ( 'A' <= c && c <= 'Z' )
@@ -1237,8 +1240,6 @@ std::string MISC::url_encode( const char* str, const size_t n, const CharCode co
         str_encoded += str_tmp;
         ++pos;
     }
-
-    if( icv ) delete icv;
 
     return str_encoded;
 }
