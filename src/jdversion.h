7cb455e806c42eeef22c6d031a1ced4d1c40383b
Masayuki Yamamoto <15698961+ma8ma@users.noreply.github.com>
2026-07-11 20:26:52 +0900

    Bump version to 0.17.0

--- src/jdversion.h
+++ src/jdversion.h
@@ -17,8 +17,8 @@
 #define MAJORVERSION 0
 #define MINORVERSION 17
 #define MICROVERSION 0
-#define JDDATE_FALLBACK    "20260620"
-#define JDTAG     "beta"
+#define JDDATE_FALLBACK    "20260711"
+#define JDTAG     ""
 
 //---------------------------------
 
